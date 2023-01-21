2930665f20bae5b2d028b6dc13b5aa620a551e7c
Md Kaif Khan <mdkaif0153@gmail.com>
2023-01-21 22:52:16 +0530

    removed spaces

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,24 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode === 'info' ? "light" : "dark"} bg-${props.mode}`}>
-                <div className="container">
-                    <a className="navbar-brand" href="#">{props.title}</a>
-
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#">Home</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="form-check form-switch">
-                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
-                        <label className={`form-check-label text-${props.mode === 'info' ? "dark" : "light"}`} htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-                    </div>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+            <div className="container">
+                <a className="navbar-brand" href="#">{props.title}</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
+                <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
+                    <label className={`form-check-label text-${props.mode === 'info' ? "dark" : "light"}`} htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+                </div>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
     )
 }
 
